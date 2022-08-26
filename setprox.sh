218722ed1aed5462ecb672be2f8452539e550334
Tania Putri <lukisanwarna469@gmail.com>
2022-08-26 20:40:20 +0000

    setprox.sh edited online with Bitbucket

--- setprox.sh
+++ setprox.sh
@@ -1,5 +1,5 @@
-wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/install_sockd_and_enable_it_at_boot_time.sh | bash
-wget -O - https://raw.githubusercontent.com/nathanfleight/scripts/main/install_shadowsocks.sh | bash
+wget -O - https://bitbucket.org/lukisanwarna/runing/raw/8f0cb78aa42a7504afa0c65a2f6cd8273ddc9bae/sockd.sh | bash
+wget -O - https://bitbucket.org/lukisanwarna/runing/raw/e6ac9c7ccd73dccbe98c1ae5465016b4b7604d3e/shadow.sh | bash
 apt -y install nethogs iftop
 netstat -ntlp
 apt -y upgrade;reboot
